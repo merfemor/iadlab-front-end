59dfc61a0d35f151085bb377592baa02a0c8979e
merfemor <efremov.roman98@gmail.com>
2017-12-18 06:30:47 +0300

    implement radius change

--- src/app/main/canvas/canvas.component.ts
+++ src/app/main/canvas/canvas.component.ts
@@ -9,31 +9,22 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class CanvasComponent implements OnInit {
     public form: FormGroup;
     public width = 400;
-    public xpxPerCell = this.width / 16.;
     public height = 400;
+    public xpxPerCell = this.width / 16.;
     public ypxPerCell = this.height / 16.;
     public radius = 1;
-
-    public fromCoordX(x) {
-        return this.width / 2. - x;
-    }
-
-    public toCoordX(x) {
-        return this.width / 2. - x;
-    }
-
-    public toCoordY(y) {
-        return this.height / 2. - y;
-    }
-
 
     constructor(private formBuilder: FormBuilder) {
     }
 
     ngOnInit() {
         this.form = this.formBuilder.group({
-            r: '1'
+            r: this.radius
         })
+    }
+
+    onChange() {
+        console.log(this.radius);
     }
 
 }
--- src/app/main/point-form/point-form.component.ts
+++ src/app/main/point-form/point-form.component.ts
@@ -1,10 +1,25 @@
-import {Component} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-point-form',
     templateUrl: 'point-form.component.html'
 })
-export class PointFormComponent {
+export class PointFormComponent implements OnInit {
     form: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            x: ['0', Validators.required],
+            y: ['0', Validators.required]
+        });
+    }
+
+    onSubmit() {
+
+    }
+
 }
