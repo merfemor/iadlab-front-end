4680d86d510bd53058cb59b336b571d9e71b5243
merfemor <efremov.roman98@gmail.com>
2017-12-18 06:18:12 +0300

    implement canvas form

--- src/app/main/canvas/canvas.component.ts
+++ src/app/main/canvas/canvas.component.ts
@@ -1,11 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
     selector: 'app-canvas',
     templateUrl: 'canvas.component.html',
     styleUrls: ['canvas.component.css']
 })
-export class CanvasComponent {
+export class CanvasComponent implements OnInit {
+    public form: FormGroup;
     public width = 400;
     public xpxPerCell = this.width / 16.;
     public height = 400;
@@ -25,6 +27,13 @@ export class CanvasComponent {
     }
 
 
-    constructor() {
+    constructor(private formBuilder: FormBuilder) {
     }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            r: '1'
+        })
+    }
+
 }
